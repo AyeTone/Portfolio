9b26d1e0b801b86ae496fbebcd615e0358137d4f
Troy_R <troyrichards28@gmail.com>
2022-01-31 15:39:15 -0500

    Added Moblie Dropdown Menu

---
+++ js/main.js
@@ -0,0 +1,16 @@
+function show_hide() {
+    document.getElementById("dd-content").classList.toggle("show")
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        } return
+      }
+    }
+}
